704f11bb10d427924311f9b4bf9f17cd2d12d64e
valentinmelchor <valentinmelchor1401@gmail.com>
2017-12-19 01:00:51 -0300

    Simple gameplay prototype
    -A simple prototype of gameplay, many things are still missing
    -empty scene renamed by name

---
+++ Zenboy/Assets/Scripts/Caller.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Caller : MonoBehaviour {
+    public float minTime, maxTime, delayInSeconds;
+    public float density;
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = FindObjectOfType<AudioSource>();
+        audioSource.Pause();
+        StartCoroutine("Call");
+    }
+
+    IEnumerator Call()
+    {
+        while (true)
+        {
+            if (!FindObjectOfType<Player>().pause)
+            {
+                yield return new WaitForSeconds(Random.Range(minTime, maxTime));
+                List<GameObject> objects = new List<GameObject>();
+                foreach (PolygonCollider2D polygonCol in FindObjectsOfType<PolygonCollider2D>())
+                {
+                    if (polygonCol.tag == "Object")
+                    {
+                        objects.Add(polygonCol.gameObject);
+                    }
+                }
+                int randomObject = Mathf.RoundToInt(Random.Range(0, objects.Count));
+                objects[randomObject].GetComponent<SpriteRenderer>().color = Color.red;
+                audioSource.transform.position = objects[randomObject].transform.position;
+                audioSource.Play();
+            }
+            yield return new WaitForSeconds(delayInSeconds);
+            if (!ObjectsAreOff())
+            {
+                FindObjectOfType<Player>().density = this.density;
+            }
+            yield return new WaitUntil(() => ObjectsAreOff());
+        }
+    }
+
+    public bool ObjectsAreOff()
+    {
+        foreach (PolygonCollider2D polygonCol in FindObjectsOfType<PolygonCollider2D>())
+        {
+            if (polygonCol.tag == "Object" && polygonCol.GetComponent<SpriteRenderer>().color == Color.red)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void DisableObjects()
+    {
+        foreach (PolygonCollider2D polygonCol in FindObjectsOfType<PolygonCollider2D>())
+        {
+            if (polygonCol.tag == "Object" && polygonCol.GetComponent<SpriteRenderer>().color == Color.red)
+            {
+                polygonCol.GetComponent<SpriteRenderer>().color = Color.white;
+            }
+        }
+        if (FindObjectOfType<AudioSource>().isPlaying)
+        {
+            FindObjectOfType<AudioSource>().Pause();
+        }
+    }
+}
---
+++ Zenboy/Assets/Scripts/Player.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+    [Range(0, 1f)]
+    [HideInInspector]
+    public float concentration = 1f;
+    [HideInInspector]
+    public float density = 0f;
+    public float recoverConcentration;
+    public Sprite[] cheers;
+    [HideInInspector]
+    public int score = 0;
+    [HideInInspector]
+    public bool pause;
+
+    private void Start()
+    {
+        StartCoroutine("Score");
+    }
+
+    private void Update()
+    {
+        GetComponent<SpriteRenderer>().sprite = GetCheerByConcentration();
+        SetConcentration();
+        UpdateBar();
+        UpdateScore();
+    }
+
+    public void RestartGame()
+    {
+        concentration = 1;
+        pause = false;
+        density = 0f;
+    }
+
+    private void SetConcentration()
+    {
+        if(concentration <= 0)
+        {
+            GameObject.Find("GameOver").GetComponent<CanvasGroup>().alpha = 1;
+            GameObject.Find("GameOver").GetComponent<CanvasGroup>().blocksRaycasts = true;
+            pause = true;
+            score = 0;
+            FindObjectOfType<Caller>().DisableObjects();
+        }
+        if(density != 0 && concentration > 0)
+        {
+            concentration -= density * Time.deltaTime;
+        } else if(density == 0 && concentration < 1)
+        {
+            concentration += recoverConcentration * Time.deltaTime;
+        } else if (concentration > 1)
+        {
+            concentration = 1;
+        } else if (concentration < 0)
+        {
+            concentration = 0;
+        }
+    }
+
+    private void UpdateBar()
+    {
+        Image bar = GameObject.Find("Bar").GetComponent<Image>();
+        bar.fillAmount = concentration;
+    }
+
+    private void UpdateScore()
+    {
+        Text text = GameObject.Find("Score").GetComponent<Text>();
+        text.text = score.ToString();
+    }
+
+    private Sprite GetCheerByConcentration()
+    {
+        for(int i = 0; i < cheers.Length; i++)
+        {
+            float portion = 1f / cheers.Length;
+            float fraction = portion * i;
+            if (concentration <= (fraction + portion) && concentration >= fraction)
+            {
+                return cheers[i];
+            }
+        }
+        return GetComponent<SpriteRenderer>().sprite;
+    }
+
+    IEnumerator Score()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            if (!pause)
+            {
+                score += 1;
+            }
+        }
+    }
+}
