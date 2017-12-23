23126cd4dba95320037d3e22b6ac1a1831b7156d
matiasbian <matiasezequielbian@gmail.com>
2017-12-23 16:28:06 -0300

    mosca agregada, usa el script de coin para ser rapido, pero si les agrada el funcionamiento podria hacerla mejor

---
+++ Zenboy/Assets/Scripts/FunctionalMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FunctionalMovement : MonoBehaviour {
+
+	public float xMultipllier;
+	public float speed;
+
+
+	void Update () {
+		transform.position =  new Vector2 (transform.position.x + (1f * speed * Time.deltaTime), Mathf.Sin (transform.position.x * xMultipllier));
+	}
+		
+	void OnBecameInvisible(){
+		Destroy (gameObject);
+	}
+}
